a1c2893f343fbcf908e9e829a5dcbaed08add75c
VINAYAN A V <vinayanav86@gmail.com>
2019-09-20 18:22:49 +0530

    Number Program Completed

---
+++ BasicModules/src/casestudy3/EmaiUpdation.java
@@ -0,0 +1,47 @@
+package casestudy3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EmaiUpdation {
+	public static int regid=0;
+	public static void main(String[] args) throws IOException {
+		
+		String name;
+		String address;
+		String contactNumber;
+		String email;
+		String proofType;
+		String proofId;
+
+		
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+	
+	System.out.println("Enter Your Name...");
+	name=br.readLine();
+	System.out.println("Enter Your Address...");
+	address=br.readLine();
+	System.out.println("Enter Your Contact Number...");
+	contactNumber=br.readLine();
+	System.out.println("Enter Your Email...");
+	email=br.readLine();
+	System.out.println("Enter Your Proof Type...");
+	proofType=br.readLine();
+	System.out.println("Enter Your Proof ID...");
+	proofId=br.readLine();
+	System.out.println("Thank You for Registering...Your Rgister Id ="+ ++regid);
+	
+	System.out.println("Do you want to continue registration (y/n)?");
+	String ch=br.readLine();
+	System.out.println("Name : "+name);
+	System.out.println("Address : "+address);
+	System.out.println("Number : "+contactNumber);
+	System.out.println("Email : "+email);
+	System.out.println("Proof Type : "+proofType);
+	System.out.println("Proof ID : "+proofId);
+
+	}
+
+}
